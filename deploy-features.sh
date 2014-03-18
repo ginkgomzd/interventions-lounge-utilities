b1163ab0d8f06bf1a92bd0f041267cc9fb757e00
Frank J. Gómez <frank@ginkgostreet.com>
2014-03-18 17:19:17 -0400

    Updated deployment script to revert feature and run db updates.

--- deploy-features.sh
+++ deploy-features.sh
@@ -8,3 +8,12 @@ drush cc all
 
 echo "Disabling masked_input module, which can't have been doing anything since it is missing the associated JS library..."
 drush -y dis masked_input
+
+# TODO: We are missing a step here whereby the Feature code is updated. Could be manual,
+# could be a git pull, could be a copy of one sort or another.
+
+echo "Updating Drupal with latest Interventions Feature..."
+drush -y features-revert intervention
+
+echo "Running database updates for latest version of Interventions module..."
+drush -y updatedb
