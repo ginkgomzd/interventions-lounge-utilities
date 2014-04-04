42b20ae7f2b9ccaad78ebd9cf3a912b60a391494
mzd <mzd@ginkgostreet.com>
2014-04-04 11:17:16 -0400

    add config for core team lead import file add moving file into place before running import

--- deploy-features.sh
+++ deploy-features.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-WEBROOT=/var/www/atd.localhost/htdocs
+WEBROOT=/var/www/staging.atd.org/htdocs
+CTL_CSV=core_team_leads_Mar25.csv
 
 cd ${WEBROOT}
 echo "Clearing caches..."
@@ -8,3 +9,20 @@ drush cc all
 
 echo "Disabling masked_input module, which can't have been doing anything since it is missing the associated JS library..."
 drush -y dis masked_input
+
+# TODO: We are missing a step here whereby the Feature code is updated. Could be manual,
+# could be a git pull, could be a copy of one sort or another.
+
+echo "Updating Drupal with latest Interventions Feature..."
+drush -y features-revert intervention
+
+echo "Running database updates for latest version of Interventions module..."
+drush -y updatedb
+
+echo "Running Normalize Target Population"
+drush intv-norm-targ
+
+echo "Running Core Team Lead update"
+cp $HOME/$CTL_CSV $WEBROOT/sites/default/files
+drush ctl-up --csv=sites/default/files/$CTL_CSV
+
